8b45d49c488041e50d6780f257afe465b4cc31c4
Craig Hurd-Rindy <gnuworldman@gmail.com>
2014-07-04 13:25:52 -0700

    Update the url, long_description, and classifiers.

--- setup.py
+++ setup.py
@@ -6,10 +6,13 @@ import os
 from distutils.core import setup
 
 
-# Declare __version_info__ and __version__ from the _version.py module code.
-exec(open(os.path.join(os.path.dirname(__file__), 'src', 'verschemes',
-                       '_version.py')).read())
+THIS_DIRECTORY = os.path.dirname(__file__)
 
+def read_file(path):
+    return open(os.path.join(THIS_DIRECTORY, path)).read()
+
+# Set __version_info__ and __version__ from the _version.py module code.
+exec(read_file(os.path.join('src', 'verschemes', '_version.py')))
 
 setup(
     name='verschemes',
@@ -18,15 +21,14 @@ setup(
     author_email="gnuworldman@gmail.com",
     maintainer="Craig Hurd-Rindy",
     maintainer_email="gnuworldman@gmail.com",
-    url='https://github.com/',
+    url='https://github.com/gnuworldman/verschemes',
     description="Version identifier management",
-    long_description="""\
-Manage version identifiers easily.
-""",
+    long_description=read_file('README'),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Topic :: Utilities',
         ],
