75880cecc072baaf5cd7fc9032d111bc5c81f180
Craig Hurd-Rindy <gnuworldman@gmail.com>
2014-07-04 13:22:09 -0700

    Replace relative imports with absolute per PEP 8. Add documentation. Create the PythonVersion.is_release property.

--- src/verschemes/python.py
+++ src/verschemes/python.py
@@ -7,15 +7,28 @@ The Python versioning module implements standard
 
 """
 
-from . import SegmentDefinition as _SegmentDefinition
-from . import SegmentField as _SegmentField
-from . import Version as _Version
+from verschemes import SegmentDefinition as _SegmentDefinition
+from verschemes import SegmentField as _SegmentField
+from verschemes import Version as _Version
 
 
 SEGMENTS = (MAJOR, MINOR, MICRO, SUFFIX) = range(4)
 
 
 class PythonMajorVersion(_Version):
+
+    """A major Python version.
+
+    This version scheme has one segment that identifies the major Python
+    version, which is only incremented for really major changes in the
+    language.  It is the base for the more detailed Python version classes and
+    is mostly only useful itself for comparison.  For example:
+
+    >>> python2 = PythonMajorVersion(2)
+    >>> my_python_version = PythonVersion('2.7.1')
+    >>> assert(my_python_version.major_version == python2)
+
+    """
 
     SEGMENT_DEFINITIONS = (
         _SegmentDefinition(),
@@ -33,6 +46,20 @@ class PythonMajorVersion(_Version):
 
 class PythonMinorVersion(PythonMajorVersion):
 
+    """A minor Python version.
+
+    This version scheme has two segments that identify the minor Python
+    version, which is incremented for less earth-shattering changes in the
+    language than a major version increment.  It is the base for the more
+    detailed Python version classes and is mostly only useful itself for
+    comparison.  For example:
+
+    >>> python33 = PythonMinorVersion(3, 3)
+    >>> my_python_version = PythonVersion(3, 4, 2)
+    >>> assert(my_python_version.minor_version > python33)
+
+    """
+
     SEGMENT_DEFINITIONS = PythonMajorVersion.SEGMENT_DEFINITIONS + (
         _SegmentDefinition(),
     )
@@ -48,6 +75,20 @@ class PythonMinorVersion(PythonMajorVersion):
 
 
 class PythonMicroVersion(PythonMinorVersion):
+
+    """A micro Python version.
+
+    This version scheme has three segments that identify the micro Python
+    version, which is incremented for each bugfix release (see `PEP 6
+    <http://legacy.python.org/dev/peps/pep-0006/>`_).  It is the base for the
+    more detailed Python version classes and is mostly only useful itself for
+    comparison.  For example:
+
+    >>> python301 = PythonMicroVersion(3, 0, 1)
+    >>> my_python_version = PythonVersion(3, 0, 1, ('b', 1))
+    >>> assert(my_python_version.micro_version == python301)
+
+    """
 
     SEGMENT_DEFINITIONS = PythonMinorVersion.SEGMENT_DEFINITIONS + (
         _SegmentDefinition(
@@ -66,6 +107,14 @@ class PythonMicroVersion(PythonMinorVersion):
 
 
 class PythonVersion(PythonMicroVersion):
+
+    """A complete, specific Python version.
+
+    This version scheme has four segments that identify a specific version of
+    Python.  See the link in the module documentation for details about the
+    Python version scheme.
+
+    """
 
     SEGMENT_DEFINITIONS = PythonMicroVersion.SEGMENT_DEFINITIONS + (
         _SegmentDefinition(
@@ -90,8 +139,28 @@ class PythonVersion(PythonMicroVersion):
     def is_nondevelopment(self):
         """Whether this version represents a non-development release.
 
-        This simply says whether it is equivalent to its `major_version`; that
+        This simply says whether it is equivalent to its `micro_version`; that
         is, whether the `SUFFIX`-index value is `None`.
+
+        >>> assert(PythonVersion('3.4.1').is_nondevelopment)
+        >>> assert(not PythonVersion('3.4.1c1').is_nondevelopment)
+        >>> assert(not PythonVersion('3.4.1+').is_nondevelopment)
 
         """
         return self[SUFFIX] is None
+
+    @property
+    def is_release(self):
+        """Whether this version represents a release.
+
+        This simply says whether the `SUFFIX`-index value is not '+'.  A '+'
+        indicates that it is an unreleased version, built directly from the
+        Subversion trunk; anything else is a release (be it development or
+        non-development).
+
+        >>> assert(PythonVersion('3.4.1').is_release)
+        >>> assert(PythonVersion('3.4.1c1').is_release)
+        >>> assert(not PythonVersion('3.4.1+').is_release)
+
+        """
+        return self[SUFFIX] != ('+', None)
