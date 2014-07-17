c941654e1aa6533d82e2d63b12e61686ea1717b1
Craig Hurd-Rindy <gnuworldman@gmail.com>
2014-07-17 09:54:44 -0700

    Import the usual stuff from __future__.

--- src/verschemes/_version.py
+++ src/verschemes/_version.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
+"""verschemes._version module"""
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
 __version_info__ = (1, 1, 0)
 __version__ = '.'.join(str(x) for x in __version_info__)
